d51eeed26a853bf4d699b22c7bdfdf069cbda6fc
Khalid Ibrahim <khalid.jambi18@gmail.com>
2021-07-15 10:29:05 +0700

    vue js looping

---
+++ main.js
@@ -0,0 +1,42 @@
+var app = new Vue({
+    el : '#app',
+    data : {
+        items :[
+            {
+                id : 1 ,
+                nama : 'Khalid Ibrahim',
+                jurusan : 'Teknik Informatika',
+                asal : 'Kota jambi',
+                phone : '+6289509939995'
+            },
+            {
+                id : 2 ,
+                nama : 'Irfan Pratama',
+                jurusan : 'Teknik Informatika',
+                asal : 'Kota Padang',
+                phone : '+6289509939995'
+            },
+            {
+                id : 3 ,
+                nama : 'Angga Permana',
+                jurusan : 'Sistem Informasi',
+                asal : 'Kota jambi',
+                phone : '+628950435395'
+            },
+            {
+                id : 4 ,
+                nama : 'Diko mahendra',
+                jurusan : 'Sistem Informasi',
+                asal : 'Lampung',
+                phone : '+628789475321'
+            },
+            {
+                id : 5 ,
+                nama : 'Miftah Farid',
+                jurusan : 'Teknik Elektronik',
+                asal : 'Bekasi',
+                phone : '+6289509939763'
+            },
+        ]
+    }
+})
